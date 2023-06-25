a302395ccb9459b45df710a95cb83b5052147204
bilecky <bilecky95@gmail.com>
2023-06-25 22:41:50 +0200

    added real time filter

--- src/components/Header/Search.jsx
+++ src/components/Header/Search.jsx
@@ -4,8 +4,10 @@ import { FiFilter, FiSearch } from 'react-icons/fi'
 import Wrapper from '../../wrapper'
 
 const Search = () => {
-	const { filterOptions, setFilterOptions } = useAppState()
+	const { setFilterOptions, offers } = useAppState()
+
 	const [showFilterModal, setShowFilterModal] = useState(false)
+	const [searchQuery, setSearchQuery] = useState('')
 
 	const handleSalarySliderChange = event => {
 		const { value } = event.target
@@ -33,55 +35,85 @@ const Search = () => {
 
 	const handleFormSubmit = event => {
 		event.preventDefault()
-		// Do something with the search query
+		applySearchQuery(searchQuery)
+	}
+
+	const handleSearchInputChange = event => {
+		setSearchQuery(event.target.value)
+		applySearchQuery(event.target.value)
+	}
+
+	const applySearchQuery = query => {
+		if (query) {
+			const filteredOffers = offers.filter(
+				offer =>
+					offer.position
+						.toLowerCase()
+						.includes(query.toLowerCase()) ||
+					offer.company
+						.toLowerCase()
+						.includes(query.toLowerCase()) ||
+					offer.location
+						.toLowerCase()
+						.includes(query.toLowerCase())
+			)
+			setFilterOptions(prevOptions => ({
+				...prevOptions,
+				searchQuery: query,
+				filteredOffers,
+			}))
+		} else {
+			setFilterOptions(prevOptions => ({
+				...prevOptions,
+				searchQuery: '',
+				filteredOffers: offers,
+			}))
+		}
 	}
 
 	return (
-		<form className=" bg-violet-700">
-    <Wrapper>
-			<h2 className='text-yellow-50 text-lg text-center py-5 font-semibold '>
-				Connecting talent. Empowering Careers. DevhireNet.
-			</h2>
-			<div className='relative w-full'>
-				<input
-					type='text'
-					className='w-full px-4 py-2 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
-					placeholder='Wyszukaj...'
-					onKeyDown={event => {
-						if (event.key === 'Enter') {
-							handleFormSubmit(event)
-						}
-					}}
-				/>
-				<FiSearch
-					className='absolute top-2 left-2 text-gray-400'
-					size={18}
-				/>
-			</div>
-			<button
-				className='w-full flex items-center justify-center mt-4 py-2 bg-blue-500 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-blue-500'
-				onClick={handleFilterClick}
-			>
-				<FiFilter size={20} className='mr-2' />
-				Filtruj
-			</button>
-			{showFilterModal && (
-				<div className='fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50'>
-					<div className='bg-white rounded-lg p-4'>
-						<h2 className='text-xl font-bold mb-4'>Filtry</h2>
-						{/* Add filter options */}
-						<button
-							className='bg-blue-500 text-white px-4 py-2 rounded-lg'
-							onClick={handleFilterModalClose}
-						>
-							Zamknij
-						</button>
-					</div>
+		<div className='bg-violet-700'>
+			<Wrapper>
+				<h2 className='text-yellow-50 text-lg text-center py-5 font-semibold'>
+					Connecting talent. Empowering Careers. DevHireNet.
+				</h2>
+				<div className='relative w-full'>
+					<form onSubmit={handleFormSubmit}>
+						<input
+							onChange={handleSearchInputChange}
+							type='text'
+							className='w-full px-4 py-2 pl-10 border border-gray-300 rounded-none focus:outline-none focus:ring-2 focus:ring-blue-500'
+							placeholder='Wyszukaj...'
+						/>
+						<FiSearch
+							className='absolute top-2 left-2 text-gray-400'
+							size={18}
+						/>
+					</form>
 				</div>
-			)}
-      </Wrapper>
-		</form>
+				<button
+					className='w-full flex items-center justify-center mt-4 py-2 bg-blue-500 rounded-none text-white focus:outline-none focus:ring-2 focus:ring-blue-500'
+					onClick={handleFilterClick}
+				>
+					<FiFilter size={20} className='mr-2' />
+					Filtruj
+				</button>
+				{showFilterModal && (
+					<div className='fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50'>
+						<div className='bg-white rounded-lg p-4'>
+							<h2 className='text-xl font-bold mb-4'>Filtry</h2>
+							{/* Add filter options */}
+							<button
+								className='bg-blue-500 text-white px-4 py-2 rounded-lg'
+								onClick={handleFilterModalClose}
+							>
+								Zamknij
+							</button>
+						</div>
+					</div>
+				)}
+			</Wrapper>
+		</div>
 	)
 }
-
 export default Search
