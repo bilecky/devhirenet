894a175635642ec613def92b38e83fb2e07342fc
bilecky <bilecky95@gmail.com>
2023-06-29 12:59:40 +0200

    add some styling and logged in user name in nav

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,119 +1,140 @@
-import React, { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { FiMoon, FiStar } from 'react-icons/fi';
-import { ImBlog } from 'react-icons/im';
-import { useAppState } from '../../context';
-import Wrapper from '../../wrapper';
-import Search from './Search';
-import { BiSolidUser } from 'react-icons/bi';
-import { Auth } from 'aws-amplify';
+import React, { useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { FiMoon, FiStar } from 'react-icons/fi'
+import { ImBlog } from 'react-icons/im'
+import { useAppState } from '../../context'
+import Wrapper from '../../wrapper'
+import Search from './Search'
+import { BiSolidUser } from 'react-icons/bi'
+import { Auth } from 'aws-amplify'
+import { AiFillInfoCircle } from 'react-icons/ai'
 
 const Navbar = () => {
-  const {
-    darkMode,
-    toggleDarkMode,
-    likedOffers,
-    isAuthenticated,
-    setIsAuthenticated,
-  } = useAppState();
-  const location = useLocation();
-  const navigate = useNavigate();
+	const {
+		darkMode,
+		toggleDarkMode,
+		likedOffers,
+		isAuthenticated,
+		setIsAuthenticated,
+		authUserName,
+	} = useAppState()
+	const location = useLocation()
+	const navigate = useNavigate()
 
-  const [showUserMenu, setShowUserMenu] = useState(false);
-  const [showLoginMessage, setShowLoginMessage] = useState(false);
+	const [showUserMenu, setShowUserMenu] = useState(false)
+	const [showLoginMessage, setShowLoginMessage] = useState(false)
 
-  const isFavoritesPage = location.pathname.includes('favorites');
-  const isDetailsPage = location.pathname.includes('offer');
-  const isLoginPage = location.pathname.includes('login');
+	const isFavoritesPage = location.pathname.includes('favorites')
+	const isDetailsPage = location.pathname.includes('offer')
+	const isLoginPage = location.pathname.includes('login')
 
-  const shouldRenderHandler = isFavoritesPage || isDetailsPage || isLoginPage;
+	const shouldRenderHandler = isFavoritesPage || isDetailsPage || isLoginPage
 
-  const handleSignOut = async () => {
-    try {
-      await Auth.signOut();
-      setIsAuthenticated(false);
-      navigate('/');
-    } catch (error) {
-      console.log('Błąd wylogowania:', error);
-    }
-  };
+	const handleSignOut = async () => {
+		try {
+			await Auth.signOut()
+			setIsAuthenticated(false)
+			navigate('/')
+		} catch (error) {
+			console.log('Błąd wylogowania:', error)
+		}
+	}
 
-  const toggleUserMenu = () => {
-    setShowUserMenu(!showUserMenu);
-  };
+	const toggleUserMenu = () => {
+		setShowUserMenu(!showUserMenu)
+	}
 
-  const handleFavoritesClick = () => {
-    if (!isAuthenticated) {
-      setShowLoginMessage(true);
-      setTimeout(() => {
-        setShowLoginMessage(false);
-        navigate('/');
-      }, 2000);
-    }
-  };
+	const handleFavoritesClick = () => {
+		if (!isAuthenticated) {
+			setShowLoginMessage(true)
+			setTimeout(() => {
+				setShowLoginMessage(false)
+				navigate('/')
+			}, 2000)
+		}
+	}
 
-  return (
-    <div className={`bg-gray-800 max-h-screen ${shouldRenderHandler ? 'h-auto' : 'lg:h-30vh'}`}>
-      <Wrapper>
-        <header className="flex items-center justify-between text-gray-900">
-          <Link to="/">
-            <div className="lg:text-4xl flex items-center text-gray-50">
-              <ImBlog />
-              <span className="ml-3 text-2xl lg:text-4xl font-oswald">DevHireNet</span>
-            </div>
-          </Link>
+	return (
+		<div
+			className={`bg-gray-800 max-h-screen ${
+				shouldRenderHandler ? 'h-auto' : 'lg:h-30vh'
+			}`}
+		>
+			<Wrapper>
+				<header className='flex items-center justify-between text-gray-900'>
+					<Link to='/'>
+						<div className='lg:text-4xl flex items-center text-gray-50'>
+							<ImBlog />
+							<span className='ml-3 text-2xl lg:text-4xl font-oswald'>
+								DevHireNet
+							</span>
+						</div>
+					</Link>
 
-          <div className="flex items-center text-gray-50">
-            <button className="mr-6 text-yellow-500 focus:outline-none" onClick={toggleDarkMode}>
-              <FiMoon size={24} />
-            </button>
-            <Link to="/favorites" className="flex items-center mr-6" onClick={handleFavoritesClick}>
-              <FiStar size={24} />
-              {isAuthenticated && <span className="ml-2 text-lg">{likedOffers?.length}</span>}
-            </Link>
-            <div className="relative">
-              <button className="flex items-center" onClick={toggleUserMenu}>
-                <BiSolidUser size={24} />
-              </button>
-              {showUserMenu && (
-              <div className="flex  justify-center text-center z-50 w-40 absolute top-10 right-0 bg-white shadow-md p-2">
-		    {isAuthenticated ? (
-			 <button
-			   className="block w-full text-center text-gray-800 py-2 px-4 hover:bg-slate-200"
-			   onClick={() => {
-				handleSignOut();
-				setShowUserMenu(false);
-			   }}
-			 >
-			   Logout
-			 </button>
-		    ) : (
-			 <Link
-			   to="/login"
-			   className="block w-full text-center text-gray-800 py-2 px-4 hover:bg-slate-200"
-			   onClick={() => setShowUserMenu(false)}
-			 >
-			   Log in
-			 </Link>
-		    )}
-		  </div>
-              )}
-            </div>
-          </div>
-        </header>
-      </Wrapper>
-      {showLoginMessage && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-75 text-white z-50">
-          <div className="bg-white rounded-md p-4">
-            <p className="text-center text-black">
-              Tylko zalogowani użytkownicy mogą dodawać oferty pracy do ulubionych.
-            </p>
-          </div>
-        </div>
-      )}
-      {!shouldRenderHandler && <Search />}
+					<div className='flex items-center text-gray-50'>
+						<button
+							className='mr-6 text-yellow-500 focus:outline-none'
+							onClick={toggleDarkMode}
+						>
+							<FiMoon size={24} />
+						</button>
+						<Link
+							to='/favorites'
+							className='flex items-center mr-6'
+							onClick={handleFavoritesClick}
+						>
+							<FiStar size={24} />
+							{isAuthenticated && (
+								<span className='ml-2 text-lg'>{likedOffers?.length}</span>
+							)}
+						</Link>
+						<div className='relative'>
+							<button className='flex items-center' onClick={toggleUserMenu}>
+								<BiSolidUser size={24} />
+								{isAuthenticated && (
+									<span className='ml-2'>{authUserName}</span>
+								)}
+							</button>
+							{showUserMenu && (
+								<div className='flex  justify-center text-center z-50 w-40 absolute top-10 right-0 bg-white shadow-md p-2'>
+									{isAuthenticated ? (
+										<button
+											className='block w-full text-center text-gray-800 py-2 px-4 hover:bg-slate-200'
+											onClick={() => {
+												handleSignOut()
+												setShowUserMenu(false)
+											}}
+										>
+											Logout
+										</button>
+									) : (
+										<Link
+											to='/login'
+											className='block w-full text-center text-gray-800 py-2 px-4 hover:bg-slate-200'
+											onClick={() => setShowUserMenu(false)}
+										>
+											Log in
+										</Link>
+									)}
+								</div>
+							)}
+						</div>
+					</div>
+				</header>
+			</Wrapper>
+			{showLoginMessage && (
+			<div className='fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-75 text-white z-50'>
+      <div className='bg-white rounded-md p-6'>
+        <AiFillInfoCircle size={48} className='text-blue-500 mx-auto mb-4' />
+        <p className='text-center text-black'>
+          Only logged-in users can view and add jobs to favourites.
+        </p>
+      </div>
     </div>
-  );
-};
+			)}
+			{!shouldRenderHandler && <Search />}
+		</div>
+	)
+}
 
-export default Navbar;
+export default Navbar
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -9,179 +9,181 @@ Amplify.configure(awsExports)
 
 
 const Login = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [code, setCode] = React.useState('');
-  const [error, setError] = React.useState('');
-  const [formType, setFormType] = React.useState('signIn');
-  const navigate = useNavigate();
-  const { checkAuth } = useAppState();
-
-  const handleSignInSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await Auth.signIn(username, password);
-      checkAuth();
-      navigate('/');
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  const handleSignUpSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await Auth.signUp({
-        username,
-        password,
-        attributes: {
-          email,
-        },
-      });
-      setFormType('confirmSignUp');
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  const handleConfirmSignUpSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await Auth.confirmSignUp(username, code);
-      await Auth.signIn(username, password);
-      checkAuth();
-      navigate('/');
-    } catch (err) {
-      setError(err.message);
-    }
-  };
-
-  if (formType === 'signIn') {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-3xl mb-4">Sign In</h1>
-        <form onSubmit={handleSignInSubmit} className="w-80">
-          <div className="mb-4">
-            <label htmlFor="username" className="block mb-2">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full border border-gray-300 p-2 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full border border-gray-300 p-2 rounded"
-            />
-          </div>
-          {error && (
-            <div className="bg-red-100 text-red-700 p-2 mb-4 rounded">{error}</div>
-          )}
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Sign In
-          </button>
-        </form>
-        <button onClick={() => setFormType('signUp')} className="mt-4 text-blue-500 hover:text-blue-600">
-          Don't have an account? Sign up
-        </button>
-      </div>
-    );
-  }
-
-  if (formType === 'signUp') {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-3xl mb-4">Sign Up</h1>
-        <form onSubmit={handleSignUpSubmit} className="w-80">
-          <div className="mb-4">
-            <label htmlFor="username" className="block mb-2">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="w-full border border-gray-300 p-2 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-2">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full border border-gray-300 p-2 rounded"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block mb-2">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full border border-gray-300 p-2 rounded"
-            />
-          </div>
-          {error && (
-            <div className="bg-red-100 text-red-700 p-2 mb-4 rounded">{error}</div>
-          )}
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Sign Up
-          </button>
-        </form>
-        <button onClick={() => setFormType('signIn')} className="mt-4 text-blue-500 hover:text-blue-600">
-          Already have an account? Sign in
-        </button>
-      </div>
-    );
-  }
-
-  if (formType === 'confirmSignUp') {
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-3xl mb-4">Confirm Sign Up</h1>
-        <form onSubmit={handleConfirmSignUpSubmit} className="w-80">
-          <div className="mb-4">
-            <label htmlFor="code" className="block mb-2">
-              Verification Code
-            </label>
-            <input
-              type="text"
-              id="code"
-              value={code}
-              onChange={(e) => setCode(e.target.value)}
-              className="w-full border border-gray-300 p-2 rounded"
-            />
-          </div>
-          {error && (
-            <div className="bg-red-100 text-red-700 p-2 mb-4 rounded">{error}</div>
-          )}
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-            Confirm
-          </button>
-        </form>
-      </div>
-    );
-  }
-};
+     const [username, setUsername] = React.useState('');
+     const [password, setPassword] = React.useState('');
+     const [email, setEmail] = React.useState('');
+     const [code, setCode] = React.useState('');
+     const [error, setError] = React.useState('');
+     const [formType, setFormType] = React.useState('signIn');
+     const navigate = useNavigate();
+     const { checkAuth } = useAppState();
+   
+     const handleSignInSubmit = async (e) => {
+       e.preventDefault();
+       try {
+         await Auth.signIn(username, password);
+         checkAuth();
+         navigate('/');
+       } catch (err) {
+         setError(err.message);
+       }
+     };
+   
+     const handleSignUpSubmit = async (e) => {
+       e.preventDefault();
+       try {
+         await Auth.signUp({
+           username,
+           password,
+           attributes: {
+             email,
+           },
+         });
+         setFormType('confirmSignUp');
+         setError('');
+       } catch (err) {
+         setError(err.message);
+       }
+     };
+   
+     const handleConfirmSignUpSubmit = async (e) => {
+       e.preventDefault();
+       try {
+         await Auth.confirmSignUp(username, code);
+         await Auth.signIn(username, password);
+         checkAuth();
+         navigate('/');
+       } catch (err) {
+         setError(err.message);
+       }
+     };
+   
+     if (formType === 'signIn') {
+       return (
+         <div className="flex flex-col items-center justify-center h-screen">
+           <h1 className="text-3xl mb-4">Sign In</h1>
+           <form onSubmit={handleSignInSubmit} className="w-80">
+             <div className="mb-4">
+               <label htmlFor="username" className="block mb-2">
+                 Username
+               </label>
+               <input
+                 type="text"
+                 id="username"
+                 value={username}
+                 onChange={(e) => setUsername(e.target.value)}
+                 className="w-full border border-gray-300 p-2 rounded"
+               />
+             </div>
+             <div className="mb-4">
+               <label htmlFor="password" className="block mb-2">
+                 Password
+               </label>
+               <input
+                 type="password"
+                 id="password"
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                 className="w-full border border-gray-300 p-2 rounded"
+               />
+             </div>
+             {error && (
+               <div className="bg-red-100 text-red-700 p-2 mb-4 rounded">{error}</div>
+             )}
+             <button type="submit" className="lg:w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+               Sign In
+             </button>
+           </form>
+           <button onClick={() => setFormType('signUp')} className="mt-4 text-blue-500 hover:text-blue-600">
+             Don't have an account? Sign up
+           </button>
+         </div>
+       );
+     }
+   
+     if (formType === 'signUp') {
+       return (
+         <div className="flex flex-col items-center justify-center h-screen">
+           <h1 className="text-3xl mb-4">Sign Up</h1>
+           <form onSubmit={handleSignUpSubmit} className="w-80">
+             <div className="mb-4">
+               <label htmlFor="username" className="block mb-2">
+                 Username
+               </label>
+               <input
+                 type="text"
+                 id="username"
+                 value={username}
+                 onChange={(e) => setUsername(e.target.value)}
+                 className="w-full border border-gray-300 p-2 rounded"
+               />
+             </div>
+             <div className="mb-4">
+               <label htmlFor="email" className="block mb-2">
+                 Email
+               </label>
+               <input
+                 type="email"
+                 id="email"
+                 value={email}
+                 onChange={(e) => setEmail(e.target.value)}
+                 className="w-full border border-gray-300 p-2 rounded"
+               />
+             </div>
+             <div className="mb-4">
+               <label htmlFor="password" className="block mb-2">
+                 Password
+               </label>
+               <input
+                 type="password"
+                 id="password"
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
+                 className="w-full border border-gray-300 p-2 rounded"
+               />
+             </div>
+             {error && (
+               <div className="bg-red-100 text-red-700 p-2 mb-4 rounded">{error}</div>
+             )}
+             <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+               Sign Up
+             </button>
+           </form>
+           <button onClick={() => setFormType('signIn')} className="mt-4 text-blue-500 hover:text-blue-600">
+             Already have an account? Sign in
+           </button>
+         </div>
+       );
+     }
+   
+     if (formType === 'confirmSignUp') {
+       return (
+         <div className="flex flex-col items-center justify-center h-screen">
+           <h1 className="text-3xl mb-4">Confirm Sign Up</h1>
+           <form onSubmit={handleConfirmSignUpSubmit} className="w-80">
+             <div className="mb-4">
+               <label htmlFor="code" className="block mb-2">
+                 Verification Code
+               </label>
+               <input
+                 type="text"
+                 id="code"
+                 value={code}
+                 onChange={(e) => setCode(e.target.value)}
+                 className="w-full border border-gray-300 p-2 rounded"
+               />
+             </div>
+             {error && (
+               <div className="bg-red-100 text-red-700 p-2 mb-4 rounded">{error}</div>
+             )}
+             <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+               Confirm
+             </button>
+           </form>
+         </div>
+       );
+     }
+   };
+   
 
 export default Login;
