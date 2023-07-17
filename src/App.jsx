c12da1bf301cc66a3c410f52165da4a562984cfb
bilecky <bilecky95@gmail.com>
2023-07-17 11:49:57 +0200

    offer route fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-	BrowserRouter as Router,
-	Route,
-	Routes,
-
-} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import '@aws-amplify/ui-react/styles.css'
 
