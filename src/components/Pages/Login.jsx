966009818dea955161c044e70bae877bd5b4af3c
bilecky <bilecky95@gmail.com>
2023-07-21 18:18:02 +0200

    import useffect

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Amplify, Auth } from 'aws-amplify'
 import { useNavigate } from 'react-router-dom'
 import { useAppState } from '../../context'
